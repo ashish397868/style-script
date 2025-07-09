f2e28e6aa1444e3ba5af1e9495d670b74349aee6
ashish <ashish397868@gmail.com>
2025-07-09 22:36:18 +0530

    Use Json file for pincodes in Pincode controller

--- server/controllers/pincodeController.js
+++ server/controllers/pincodeController.js
@@ -1,10 +1,18 @@
 // server/controllers/pincodeController.js
-const pincodes = {
-  "135001": ["yamunanagar", "haryana"],
-  "110003": ["delhi", "delhi"],
-  "160001": ["chandigarh", "chandigarh"]
-};
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 60 * 60*24 }); // 1 day
+const fs = require("fs");
+const path = require("path");
 
 exports.getPincode = (req, res) => {
-  res.json(pincodes);
+  const cacheKey = "all-pincodes";
+  const cached = cache.get(cacheKey);
+  if (cached) return res.json(cached);
+
+  const dataPath = path.join(__dirname, "../data/pincodes.json");
+  const raw = fs.readFileSync(dataPath);
+  const pincodes = JSON.parse(raw);
+
+  cache.set(cacheKey, pincodes);
+  return res.json(pincodes);
 };
